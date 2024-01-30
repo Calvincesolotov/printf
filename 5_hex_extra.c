a95f927470a8b09086a7ddc673f088712ee02f6b
Calvincesolotov <solotovtechn@gmail.com>
2024-01-30 10:10:08 +0300

    modified print-hex extra

--- 5_hex_extra.c
+++ 5_hex_extra.c
@@ -1,36 +1,42 @@
 #include "main.h"
 
 /**
- * print_hex_extra - prints an hexgecimal number.
- * @num: arguments.
- * Return: counter.
+ * print_hex_extra - Prints a hexadecimal representation of a number.
+ * @num: The number to be printed in hexadecimal.
+ *
+ * Return: The number of characters printed.
  */
 int print_hex_extra(unsigned long int num)
 {
-	long int i;
-	long int *array;
-	long int counter = 0;
-	unsigned long int temp = num;
+    long int i;
+    long int *hexArray;
+    long int counter = 0;
+    unsigned long int temp = num;
 
-	while (num / 16 != 0)
-	{
-		num /= 16;
-		counter++;
-	}
-	counter++;
-	array = malloc(counter * sizeof(long int));
+    /* Count the number of digits in hexadecimal representation */
+    while (num / 16 != 0)
+    {
+        num /= 16;
+        counter++;
+    }
+    counter++;
+    hexArray = malloc(counter * sizeof(long int));
 
-	for (i = 0; i < counter; i++)
-	{
-		array[i] = temp % 16;
-		temp = temp / 16;
-	}
-	for (i = counter - 1; i >= 0; i--)
-	{
-		if (array[i] > 9)
-			array[i] = array[i] + 39;
-		_putchar(array[i] + '0');
-	}
-	free(array);
-	return (counter);
+    /* Populate the array with hexadecimal digits */
+    for (i = 0; i < counter; i++)
+    {
+        hexArray[i] = temp % 16;
+        temp = temp / 16;
+    }
+
+    /* Print the hexadecimal representation */
+    for (i = counter - 1; i >= 0; i--)
+    {
+        if (hexArray[i] > 9)
+            hexArray[i] = hexArray[i] + 39;
+        _putchar(hexArray[i] + '0');
+    }
+
+    free(hexArray);  /* Free allocated memory */
+    return (counter);  /* Return the number of characters printed */
 }
