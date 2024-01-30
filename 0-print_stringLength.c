bf68fa7e5cd493d651c554cbc71660b31752e45e
Calvincesolotov <solotovtechn@gmail.com>
2024-01-30 09:09:17 +0300

    modified length

--- 0-print_stringLength.c
+++ 0-print_stringLength.c
@@ -8,11 +8,11 @@
 
 int _strlen(char *s)
 {
-	int c;
+	int length;
 
-	for (c = 0; s[c] != 0; c++)
+	for (length = 0; s[length] != 0; length++)
 		;
-	return (c);
+	return (length);
 
 }
 
@@ -24,10 +24,10 @@ int _strlen(char *s)
 
 int _strlenc(const char *s)
 {
-	int c;
+	int length;
 
-	for (c = 0; s[c] != 0; c++)
+	for (length = 0; s[length] != 0; length++)
 		;
-	return (c);
+	return (length);
 }
 
