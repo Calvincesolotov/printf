d9429fcbc7953c21feb7a42e332956c7059023b8
Calvincesolotov <solotovtechn@gmail.com>
2024-01-30 09:38:15 +0300

    modified file rot 13

--- 14-print_rot13.c
+++ 14-print_rot13.c
@@ -1,37 +1,47 @@
 #include "main.h"
+
 /**
- * print_rot13 - convert to rot13
- * @args: printf arguments
- * Return: counter
+ * print_rot13 - Convert input string to ROT13 and print it.
+ * @args: Arguments for printf
  *
+ * Return: Number of characters printed.
  */
 int print_rot13(va_list args)
 {
 	int i, j, counter = 0;
-	int k = 0;
-	char *s = va_arg(args, char*);
-	char al[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	char bl[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+	int found_match = 0;
+	int shift = 13;
+	char *input_str = va_arg(args, char*);
+	char alphabet[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char rot13_alphabet[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
-	if (s == NULL)
-		s = "(null)";
-	for (i = 0; s[i]; i++)
+	/* Handle NULL input */
+	if (input_str == NULL)
+		input_str = "(null)";
+
+	/* Iterate through the input string */
+	for (i = 0; input_str[i]; i++)
 	{
-		k = 0;
-		for (j = 0; al[j] && !k; j++)
+		found_match = 0;
+
+		/* Check if the character is in the alphabet */
+		for (j = 0; alphabet[j] && !found_match; j++)
 		{
-			if (s[i] == al[j])
+			if (input_str[i] == alphabet[j])
 			{
-				_putchar(bl[j]);
+				_putchar(rot13_alphabet[j]);
 				counter++;
-				k = 1;
+				found_match = 1;
 			}
 		}
-		if (!k)
+
+		/* If the character is not in the alphabet, print it unchanged */
+		if (!found_match)
 		{
-			_putchar(s[i]);
+			_putchar(input_str[i]);
 			counter++;
 		}
 	}
+
 	return (counter);
 }
