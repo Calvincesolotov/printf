a452db8b4c9ae30ac916f6488dc559163d3d6aa0
Calvincesolotov <solotovtechn@gmail.com>
2024-01-30 07:50:53 +0300

    changed print.c to print_character

---
+++ 0-print_character.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_character - Prints a character.
+ *
+ * @val: The argument list containing the character to be printed.
+ *
+ * Description: This function takes a character from the argument list and
+ *              prints it using the _putchar function.
+ *
+ * Return: 1 - Always returns 1.
+ */
+int print_character(va_list val)
+{
+    char character;
+
+    character = va_arg(val, int);
+    _putchar(character);
+    return (1);
+}
+
+
