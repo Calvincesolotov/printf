a0a5f928cb8583710a3e7678464ec1d9febef409
Calvincesolotov <solotovtechn@gmail.com>
2024-01-30 07:30:53 +0300

    modified task %.c

--- 0-print_%.c
+++ 0-print_%.c
@@ -1,11 +1,17 @@
 #include "main.h"
+
 /**
- * print_37 - prints the char %.
- * Return: 1.
+ * print_37 - Prints the character '%'.
+ *
+ * Description: This function prints the character '%' using the _putchar
+ *              function.
+ *
+ * Return: 1 - Always returns 1.
  */
 int print_37(void)
 {
-	_putchar(37);
+	_putchar(37); /* ASCII code for '%' */
 	return (1);
 }
+
 
