2fd3be900810a2b8b202980d3581d61031004580
Calvincesolotov <solotovtechn@gmail.com>
2024-01-30 07:27:08 +0300

    modified print_37

--- main.h
+++ main.h
@@ -13,8 +13,7 @@ typedef struct format
 
 
 int _printf(const char *format, ...);
-int printPercentSymbol(void);
-/*int print_37(void);*/
+int print_37(void);
 int _strlen(char *s);
 int _strlenc(const char *s);
 int print_s(va_list val);
