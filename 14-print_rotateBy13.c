c1b45afc081044d0c3eaf2f7cd603b5a31595068
Calvincesolotov <solotovtechn@gmail.com>
2024-01-30 09:49:06 +0300

    changed file

--- 14-print_rotateBy13.c
+++ 14-print_rotateBy13.c
@@ -1,37 +1,41 @@
 #include "main.h"
+
 /**
- * print_rot13 - convert to rot13
+ * print_rotateBy13 - Convert to rot13
  * @args: printf arguments
- * Return: counter
- *
+ * Return: Counter
  */
 int print_rotateBy13(va_list args)
 {
 	int i, j, counter = 0;
-	int k = 0;
+	int found_match = 0;  /* Flag to check if a character is found in 'al' array */
 	char *s = va_arg(args, char*);
-	char al[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	char bl[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+	char al[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char bl[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	if (s == NULL)
 		s = "(null)";
+
 	for (i = 0; s[i]; i++)
 	{
-		k = 0;
-		for (j = 0; al[j] && !k; j++)
+		found_match = 0;  /* Reset the flag for each character in 's' */
+
+		for (j = 0; al[j] && !found_match; j++)
 		{
 			if (s[i] == al[j])
 			{
 				_putchar(bl[j]);
 				counter++;
-				k = 1;
+				found_match = 1;  /* Set the flag to indicate a match */
 			}
 		}
-		if (!k)
+
+		if (!found_match)
 		{
 			_putchar(s[i]);
 			counter++;
 		}
 	}
+
 	return (counter);
 }
